dbf9fe936803da2e2cc22ef435e54ed352195b5e
woachkatzl <y.v.polonskaya@gmail.com>
2023-08-28 19:41:08 -0400

    task 2 complete

---
+++ task-2/index.js
@@ -0,0 +1,43 @@
+const blogContainer = document.querySelector(".container");
+
+// Функция создать новый пост
+const makePost = (postObject) => {
+    //create html elements
+    const newPost = document.createElement("div");
+    const postTitle = document.createElement("h2");
+    const postBody = document.createElement("p");
+
+    //fill in content
+    postTitle.textContent = postObject.title;
+    postBody.textContent = postObject.body;
+
+    //style elements
+    newPost.classList.add("my-3");
+    postTitle.classList.add("fs-4", "text-primary");
+
+    newPost.append(postTitle, postBody);
+    return newPost;
+};
+
+// Функция добавить пост на страницу
+const addPost = (post) => {
+    blogContainer.append(post);
+}
+
+// Функция заполнить страницу постами из API
+const populatePosts = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/");
+        const postsData = await response.json();
+
+        postsData.forEach(postObject => {
+            const newPost = makePost(postObject);
+            addPost(newPost);
+        })
+    }
+    catch (error) {
+        console.log("Ошибка: ", error);
+    }
+};
+
+window.addEventListener("DOMContentLoaded", populatePosts);
